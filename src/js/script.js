5ce3844a7fa33d7c7f7189b8c8e3f8559a1db121
vuhang1712 <vuhang1939@gmail.com>
2022-03-08 00:16:41 +0700

    Làm chức năng sắp xếp sản phẩm

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-import {loadData} from '/src/js/pages/product.js';
+import {showProducts} from '/src/js/pages/product.js';
 import {loadProductCart} from '/src/js/pages/cart.js';
 import {payment} from '/src/js/pages/payment.js';
 import {confirmOrder} from '/src/js/pages/confirm-order.js';
 
-loadData();
+showProducts();
 loadProductCart();
 payment();
 confirmOrder();
